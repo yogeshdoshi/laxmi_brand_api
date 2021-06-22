afaceff98c0766c3a30b813e85593cb60eee2a94
pallavi2186 <f201506100110066@gmail.com>
2021-06-22 23:45:43 +0530

    upload image

--- application/controllers/api/admin/Uploadimages.php
+++ application/controllers/api/admin/Uploadimages.php
@@ -11,27 +11,33 @@ class Uploadimages extends CI_Controller
 		$this->load->database();
 		$this->load->model('admin/UploadModel');
 	}
-	
+
 	public function  index()
 	{
+		$image_for = $_POST['image_for'];
+		$reference_id = $_POST['reference_id'];
+		$uniqueId = time() . '-' . mt_rand();
+		$imgname = "";
+		$imgpath = "";
 
-		// print_r($_POST);die;
-
-		$image_for=$_POST['image_for'];
-		$reference_id=$_POST['reference_id'];
-
+		if ($image_for == 1) {
+			$imgpath = "product/image";
+			$imgname = "PR_" . $reference_id . "_" . $uniqueId;
+		} else if ($image_for == 2) {
+			$imgpath = "category/image";
+			$imgname = "CA_" . $reference_id . "_" . $uniqueId;
+		}
 
 		$this->load->helper('url');
-
 		$data = array();
-		
+
 		if ($_FILES != NULL) {
 			if (!empty($_FILES['file']['name'])) {
-				
-				$config['upload_path'] = ASSETS_PATH;
+
+				$config['upload_path'] = ASSETS_PATH . $imgpath;
 				$config['allowed_types'] = 'jpg|jpeg|png|gif';
 				$config['max_size'] = '2000'; // max_size in kb that means 2mb
-				$config['file_name'] = $_FILES['file']['name'];
+				$config['file_name'] = $imgname;
 
 				$this->load->library('upload', $config);
 
@@ -39,35 +45,30 @@ class Uploadimages extends CI_Controller
 					$error = array('error' => $this->upload->display_errors());
 					$result["message"] = "Something went wrong";
 					$this->responsedata(400, 'failed', $result);
-		
 				} else {
 					$data = array('image_metadata' => $this->upload->data());
-				
 					$datas = array(
 						'image_for' => $image_for,
 						'reference_id' => $reference_id,
-						'image_path' => $data['image_metadata']['file_path'],
+						'image_name' => $data['image_metadata']['orig_name'],
+						'image_path' => $data['image_metadata']['full_path'],
 						'image_type' => $data['image_metadata']['image_type'],
 						'created_at' => CURRENT_DATETIME
 					);
-
+					
 					$this->UploadModel->save_upload($datas);
-
-					$result["message"]="successfully uploaded files";
+					$result["message"] = "successfully uploaded files";
 					$this->responsedata(200, 'success', $result);
-		
 				}
-		} else {
-			
-			$result["message"] = "Something went wrong";
-		$this->responsedata(400, 'failed', $result);
+			} else {
+				$result["message"] = "Something went wrong";
+				$this->responsedata(400, 'failed', $result);
+			}
 		}
-	}
 	}
 
 	public function responsedata($status_code, $status, $data)
-    {
-        json_output($status_code, array('status' => $status, 'data' => $data));
-		
-    }
+	{
+		json_output($status_code, array('status' => $status, 'data' => $data));
+	}
 }
